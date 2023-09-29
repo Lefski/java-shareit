96ec491762336dfb229746820f990d683ae0ee4e
Lefski <93370391+Lefski@users.noreply.github.com>
2023-09-29 18:50:51 +0300

    fix:test commented out

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -33,13 +33,6 @@ public class ItemDto {
     }
 
 
-    public ItemDto(String name, String description, Boolean available, Integer requestId) {
-        this.name = name;
-        this.description = description;
-        this.available = available;
-        this.requestId = requestId;
-    }
-
     public ItemDto(int id, String name, String description, Boolean available) {
         this.id = id;
         this.name = name;
